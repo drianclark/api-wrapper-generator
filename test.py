096ef4daca6cc93f2c59dcd05bec3f5494a35285
drianclark <dreiclark@gmail.com>
2020-08-21 13:26:08 +0100

    updated gitignore

--- test.py
+++
@@ -1,9 +0,0 @@
-from renders.renders import Renders
-from pprint import pprint
-
-r = Renders()
-
-m = r.stations(exists=['measures.period'])
-
-pprint(m[0].measures())
-# print(type(m[0].measures().period()))
